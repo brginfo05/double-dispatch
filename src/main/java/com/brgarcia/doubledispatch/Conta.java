943620319ec862c2bb8324851175f2d5d419920d
bruno <brginfo05@gmail.com>
2016-01-14 19:36:12 -0200

    apache commons

--- src/main/java/com/brgarcia/doubledispatch/Conta.java
+++ src/main/java/com/brgarcia/doubledispatch/Conta.java
@@ -1,5 +1,7 @@
 package com.brgarcia.doubledispatch;
 
+import org.apache.commons.lang3.Validate;
+
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
@@ -9,7 +11,11 @@ public class Conta {
     private BigDecimal valor;
     private List<Parcela> parcelas;
 
+    // Deve garantir as constraints ao construir o objeto
     public Conta(BigDecimal valor) {
+        Validate.notNull(valor, "Deve informar o valor da conta");
+        Validate.isTrue(valor.compareTo(valor) > 0, "Valor da conta deve ser maior que zero");
+
         this.valor = valor;
     }
 
@@ -17,13 +23,23 @@ public class Conta {
         return valor;
     }
 
+    // Não deve permitir que o estado interno seja modificado por outras classes
     public List<Parcela> getParcelas() {
-        return parcelas;
+        return Collections.unmodifiableList(parcelas);
     }
 
+    // Toda operação deve garantir que o objeto continue em um estado consistente
     public void parcelar(Parcelador parcelador) {
+        // Uso de double dispatch. Garante que o domain service nao altere o estado interno do model
         List<Parcela> parcelasGeradas = parcelador.parcelar(this);
 
+        // Garantindo consistencia
+        validar(parcelasGeradas);
+
+        parcelas = parcelasGeradas;
+    }
+
+    private void validar(List<Parcela> parcelasGeradas) {
         BigDecimal somaParcelas = parcelasGeradas
                 .stream()
                 .map(Parcela::getValor)
@@ -32,7 +48,5 @@ public class Conta {
         if(somaParcelas.compareTo(valor) != 0) {
             throw new RuntimeException("A soma das parcelas esta diferente do valor da conta");
         }
-
-        parcelas = Collections.unmodifiableList(parcelasGeradas);
     }
 }
--- src/main/java/com/brgarcia/doubledispatch/Main.java
+++ src/main/java/com/brgarcia/doubledispatch/Main.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class Main {
 
     public static void main(String[] args) {
-        new Conta(BigDecimal.TEN)
-                .parcelar(new ParceladorNumeroParcelas(2));
+        Conta conta = new Conta(BigDecimal.TEN);
+        conta.parcelar(new ParceladorNumeroParcelas(2));
     }
 }
